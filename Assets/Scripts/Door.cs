3b63ac5ca2808373c31d7d70f107eeaaef4e7f65
florinaandreea <fion14@student.aau.dk>
2017-03-21 15:19:26 +0100

    door thingie
    door thingie

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+	public GameObject WinningText
+	public void Win()
+	{
+		WinningText.SetActive (true);
+		GameManager.ResetLevel;
+	}
+
+	void OnTriggerEnter()
+	{
+		GameManager.instance.Win ();
+	}
+
+
+
+}
